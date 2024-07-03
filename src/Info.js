934f4876ed1775b751232409edf4a44f15a48db4
Arjun059 <arjunchitara35081@gmail.com>
2024-07-03 23:02:17 +0530

    chan

---
+++ src/Info.js
@@ -0,0 +1,15 @@
+
+// Filename - Info.js
+ 
+// Importing the css for the info
+ 
+const Info = () => {
+    return (
+        <div className="info">
+            <div className="player">Player 1: X</div>
+            <div className="player">Player 2: O</div>
+        </div>
+    );
+};
+ 
+export default Info;
